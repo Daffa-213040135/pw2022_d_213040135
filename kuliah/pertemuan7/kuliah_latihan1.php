d06f3d5db89b18cb113682291cbb497b1d99b553
Daffa-213040135 <daffa.213040135@mail.unpas.ac.id>
2022-03-29 23:20:48 +0700

    update semua tugas pertemuan

---
+++ kuliah/pertemuan7/kuliah_latihan1.php
@@ -0,0 +1,24 @@
+<?php 
+    // Variabel Global
+    // $nama = "Sandhika"
+
+    // function cetakNama($x) {
+    //     return "Halo $x";
+
+    // }
+    // echo cetakNama('Sandhika ');
+
+    // $nama = '';
+    
+    // echo $nama;
+
+    // SUPERGLOBALS
+    // Variabel PHP yang bisa kita pakai dimanapun
+    // Bentuknya adalah Array Associative ( Indexnya String )
+    // $_GET
+    // $_POST
+    // $_REQUEST
+    // $_SERVER
+    // var_dump($_SERVER["SERVER_NAME"]);
+    
+?> 
